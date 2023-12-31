43591aa068eacb25d32a2db7a549d0a48389dfb3
Satish Kumar Singh <satishkrsingh91@gmail.com>
2023-12-31 23:42:03 +0530

    Added car Images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import {Hero, CustomFilter, SearchBar, CarCard } from "@/components";
+import { Hero, CustomFilter, SearchBar, CarCard } from "@/components";
 import { fetchCars } from "@/utils";
 
 export default async function Home() {
   const allCars = await fetchCars();
-  console.log(allCars);
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -13,9 +12,10 @@ export default async function Home() {
 
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
-          <h1 className="text-4xl font-bold">Car Catalogues</h1>
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
         </div>
+
         <div className="home__filters">
           <SearchBar />
 
@@ -23,21 +23,22 @@ export default async function Home() {
             <CustomFilter title="fuel" />
             <CustomFilter title="year" />
           </div>
-
-          {!isDataEmpty ? (
-            <section>
-              <div className="home__cars-wrapper"></div>
-              {allCars?.map((car) => {
-                return <CarCard  car={car} />;
-              })}
-            </section>
-          ) : (
-            <div className="home__error-conainer">
-              <h2 className="text-black text-x1 font-bold">No cars</h2>
-              <p>{allCars?.message}</p>
-            </div>
-          )}
         </div>
+        
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+            {allCars?.map((car) => {
+              return <CarCard car={car} />;
+            })}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-x1 font-bold">No cars</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@/types";
+
 export const fetchCars = async () => {
   const headers = {
     "X-RapidAPI-Key": "3ca2b259b1mshbfbce5556a26f3fp1d62c8jsn8b240c693559",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=bmw",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
     {
       headers: headers,
     }
@@ -27,3 +29,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car : CarProps, angle? : string) =>{
+  const url = new URL('https://cdn.imagin.studio/getimage');
+  
+  const { make, year, model} = car;
+
+  url.searchParams.append('customer', 'hrjavascript-mastery');
+
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+}
